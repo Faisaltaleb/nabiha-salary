4922b003d70dc3b47997d5fb1f6f280ab90ec53f
AymanMohanna <aymangg042@gmail.com>
2025-02-21 21:16:10 +0200

    added variables

---
+++ nabiha-salary.py
@@ -0,0 +1,5 @@
+salary = float(input("Enter your salary:"))
+month = input ("Enter the month that yu're storing the salary:")
+savings_percentage = float(input("Enter the percentage for savings :"))
+electricity_percentage=float(input("Enter the percentage for rent:"))
+rent_percentage=float(input("Enter the percentage for the rent:"))
