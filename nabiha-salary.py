147e856873599f145d9c5920e90ed2d9d9bfeab9
AymanMohanna <aymangg042@gmail.com>
2025-02-21 21:23:15 +0200

    added calculations

--- nabiha-salary.py
+++ nabiha-salary.py
@@ -3,3 +3,10 @@ month = input ("Enter the month that yu're storing the salary:")
 savings_percentage = float(input("Enter the percentage for savings :"))
 electricity_percentage=float(input("Enter the percentage for rent:"))
 rent_percentage=float(input("Enter the percentage for the rent:"))
+
+savings = (savings_percentage/100)*salary
+rent =(rent_percentage/100)*salary
+electricity= (electricity_percentage/100)*salary
+
+totalexpenses= savings + rent + electricity
+
