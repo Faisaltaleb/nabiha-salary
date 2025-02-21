6c280c21c9d0717569f065179b6e75a05cd243f5
AymanMohanna <aymangg042@gmail.com>
2025-02-21 21:45:08 +0200

    printed the results

--- nabiha-salary.py
+++ nabiha-salary.py
@@ -19,3 +19,19 @@ salarysq= salary**2
 
 extra_savings= 50
 savings_division=extra_savings/savings
+
+print ("/nYour salary is ",salary,"in the month of ",month)
+print ("/nYour savings for ",month," is ",savings)
+print ("/nYour rent for ",month," is ",rent)
+print ("/nYour electricity for ",month ," is ",electricity)
+print ("/nYour total expenses for " ,month, "is" ,totalexpenses)
+print("/n After these expeneses the amout remaining is :",remainder)
+print("/nThe yearly rent cost is ",yearlyrent,"and the yealry elecricity expenses is ",yearlyelec)
+print("/nYour salary raised for the power of two JUST FOR FUN is ",salarysq)
+print("/nAdditional savings of 50$ dividedby your savings is ",savings_division)
+
+
+
+
+
+
